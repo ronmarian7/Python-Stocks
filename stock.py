214d9595ac3fe708190813ca675b70430c578534
ronmarian7 <ronmarian7@gmail.com>
2022-07-22 12:36:20 +0300

    This is the first edition

---
+++ stock.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def plot_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price ($)", "Historical Revenue ($)"), vertical_spacing = .5)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($ Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False, height=1000, title=stock, xaxis_rangeslider_visible=True)
+    fig.show()
+
+    tesla_data = yf.Ticker('TSLA')
+
+    # history function helps to extract stock information.
+    # setting period parameter to max to get information for the maximum amount of time.
+    tsla_data = tesla_data.history(period='max')
+
+    # Resetting the index
+    tsla_data.reset_index(inplace=True)
+
+    # display the first five rows
+    tsla_data.head()
